50a79433ef017c4a1b7eecd8695bb864c3840f53
rushikeshgadekar <rushikeshgadekar17399@gmail.com>
2021-04-01 23:43:58 +0530

    Initial commit - Navbar + Carousel

---
+++ pages/scroll.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  //for tabs show hide
+  $(".tab_click li a")
+    .on("click", function (e) {
+      $(this.hash).fadeIn(200).siblings().fadeOut(200);
+      e.preventDefault();
+    })
+    .first()
+    .click();
+
+  // for active class
+  $(".tab_click li").on("click", function () {
+    $(".tab_click li.current").removeClass("current");
+    $(this).addClass("current");
+  });
+});
+/** Smooth Scrolling Functionality **/
+$(document).ready(function () {
+  // Init controller
+  var controller = new ScrollMagic.Controller();
+  // Change behavior of controller
+  // to animate scroll instead of jump
+  controller.scrollTo(function (target) {
+    TweenMax.to(window, 2, {
+      scrollTo: {
+        y: target - 65, // scroll position of the target along y axis
+        autoKill: true, // allows user to kill scroll action smoothly
+      },
+      ease: Cubic.easeInOut,
+    });
+  });
+  //  Bind scroll to anchor links
+  $(document).on("click", "a[href^=#]", function (e) {
+    var id = $(this).attr("href");
+    if ($(id).length > 0) {
+      e.preventDefault();
+      // trigger scroll
+      controller.scrollTo(id);
+      // If supported by the browser we can also update the URL
+      if (window.history && window.history.pushState) {
+        history.pushState("", document.title, id);
+      }
+    }
+  });
+});
